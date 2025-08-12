728f6804ab3796ef75a687ea828bf35a6d78440e
samoksha-jayasinghe <samokshasri1@gmail.com>
2025-08-12 09:44:06 +0530

    9th commit (UserBO and UserBOImpl)

--- src/main/java/lk/ijse/backery_management_system/bo/custom/UserBO.java
+++ src/main/java/lk/ijse/backery_management_system/bo/custom/UserBO.java
@@ -8,11 +8,11 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public interface UserBO extends SuperBO {
-    public String getNextId() throws SQLException;
-    public boolean save(UsersDto entity) throws SQLException;
-    public  boolean update(UsersDto entity) throws SQLException;
-    public boolean delete(String userId) throws SQLException;
-    public ArrayList<UsersDto> search(String search) throws SQLException;
-    public ArrayList<UsersDto> getAll() throws SQLException;
+    public String getNextId() throws SQLException, ClassNotFoundException;
+    public boolean save(UsersDto entity) throws SQLException, ClassNotFoundException;
+    public  boolean update(UsersDto entity) throws SQLException, ClassNotFoundException;
+    public boolean delete(String userId) throws SQLException, ClassNotFoundException;
+    public ArrayList<UsersDto> search(String search) throws SQLException, ClassNotFoundException;
+    public ArrayList<UsersDto> getAll() throws SQLException, ClassNotFoundException;
 
 }
--- src/main/java/lk/ijse/backery_management_system/bo/custom/impl/UserBOImpl.java
+++ src/main/java/lk/ijse/backery_management_system/bo/custom/impl/UserBOImpl.java
@@ -1,4 +1,54 @@
 package lk.ijse.backery_management_system.bo.custom.impl;
 
-public class UserBOImpl {
+import lk.ijse.backery_management_system.bo.custom.UserBO;
+import lk.ijse.backery_management_system.dao.DAOFactory;
+import lk.ijse.backery_management_system.dao.custom.SupplierDAO;
+import lk.ijse.backery_management_system.dao.custom.UsersDAO;
+import lk.ijse.backery_management_system.dto.SupplierDto;
+import lk.ijse.backery_management_system.dto.UsersDto;
+import lk.ijse.backery_management_system.entity.SupplierEntity;
+import lk.ijse.backery_management_system.entity.UsersEntity;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class UserBOImpl implements UserBO {
+    private UsersDAO usersDAO = (UsersDAO) DAOFactory.getInstance().getDAO(DAOFactory.DAOTypes.USERS);
+
+    public String getNextId() throws SQLException, ClassNotFoundException {
+        return usersDAO.getNextId();
+    }
+
+
+    public boolean save(UsersDto usersDto) throws SQLException, ClassNotFoundException {
+        return usersDAO.save(new UsersEntity(usersDto.getUserId(),usersDto.getName(),usersDto.getAddress(),usersDto.getEmail(),usersDto.getContact(),usersDto.getPassword()));
+    }
+
+    public boolean update(UsersDto usersDto) throws SQLException, ClassNotFoundException {
+        return usersDAO.update(new UsersEntity(usersDto.getUserId(),usersDto.getName(),usersDto.getAddress(),usersDto.getEmail(),usersDto.getContact(),usersDto.getPassword()));
+    }
+
+    public boolean delete(String userId) throws SQLException, ClassNotFoundException {
+        return usersDAO.delete(userId);
+    }
+
+    public ArrayList<UsersDto> search(String search) throws SQLException, ClassNotFoundException {
+        ArrayList<UsersEntity> users = usersDAO.search(search);
+        ArrayList<UsersDto> dtos = new ArrayList<>();
+        for (UsersEntity user : users) {
+            dtos.add(new UsersDto(user.getUserId(),user.getName(),user.getAddress(),user.getEmail(),user.getContact(),user.getPassword()));
+        }
+        return dtos;
+    }
+
+    public ArrayList<UsersDto> getAll() throws SQLException, ClassNotFoundException {
+        ArrayList<UsersEntity> users = usersDAO.getAll();
+        ArrayList<UsersDto> usersDtos = new ArrayList<>();
+        for (UsersEntity user : users) {
+            usersDtos.add(new UsersDto(user.getUserId(),user.getName(),user.getAddress(),user.getEmail(),user.getContact(),user.getPassword()));
+        }
+
+        return usersDtos;
+    }
 }
+
